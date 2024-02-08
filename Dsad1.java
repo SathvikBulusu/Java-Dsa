450f89f7e3cb8d6512e76a53be5310fdafe64d42
Sathvik_Bulusu <bulususathvik7890@gmail.com>
2024-02-08 11:04:49 +0530

    another one

--- Dsad1.java
+++ Dsad1.java
@@ -1,5 +1,11 @@
 public class Dsad1{
     public static void main(String[] args) {
         System.out.println("hello there its just a common program check");
+
+        String greeting = "hello";
+        String good = "broke";
+        String bloke = "nirobi";
+
+        System.out.println(greeting+bloke+good);
     }
 }
---
+++ Dsad1arrays.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Dsad1arrays{
+    public static void main(String[] args) {
+        int good[] = new int[6];
+
+        Scanner sn = new Scanner(System.in);
+        System.out.println("enter the elements");
+        for (int i = 0; i<good.length ; i++) {
+            good[i] = sn.nextInt();
+        }
+        System.out.println("elements are");
+        for (int i=0;i<good.length;i++){
+            System.out.println(good[i]);
+        }
+    }
+}
