020d02396b4d24d8832abbaa5730617521670064
Sathvik_Bulusu <bulususathvik7890@gmail.com>
2024-02-21 10:44:15 +0530

    java boi

---
+++ aki.java
@@ -0,0 +1,10 @@
+public class aki {
+    public static void main(String[]args){
+      int a = 65;
+      char b = (char) a;
+        System.out.println(a);
+    }
+}
+ // so i been learning about the type conversions which is absolutely awesome
+// i got to know that byte has = 5 with implicit understanding
+// i got ot know short = 4,  int = 3 so it cant turn to char, float = 2 , and then char has backwards understanding untill int
