ba29ec112f1b6d52b8e2b2cc00796f3fa3a9ab95
Sathvik_Bulusu <bulususathvik7890@gmail.com>
2024-02-21 22:06:01 +0530

    a program

---
+++ ebfhvio.java
@@ -0,0 +1,18 @@
+public class ebfhvio {
+     int a, b, c;
+
+    int add() {
+        a=21;
+        b=54;
+        c = a + b;
+        return c;
+
+    }
+
+    public static void main(String[] args) {
+        ebfhvio i = new ebfhvio();
+        System.out.println(i.add());
+    }
+
+
+}
