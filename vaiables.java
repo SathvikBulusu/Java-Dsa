909d27d13d8381b4d1e1940d6ac4cbc58e41617c
Sathvik_Bulusu <bulususathvik7890@gmail.com>
2024-02-21 11:36:15 +0530

    variables and its understanding

---
+++ vaiables.java
@@ -0,0 +1,19 @@
+public class vaiables {
+    static int a = 10;
+    int b =12;
+
+    public static void main(String[] args) {
+        vaiables onj = new vaiables();
+        vaiables.a=100;
+        onj.a=25;
+        new vaiables().b=35;
+        new vaiables().a=75;
+        new vaiables().b=35;
+        System.out.println(a);
+        System.out.println(jelly.dost);
+    }
+}
+
+class jelly{
+    static int dost = 67597;
+}
