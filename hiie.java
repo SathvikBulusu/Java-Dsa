f28c2a7742aafebde7177f301a27cc7d5df38f5b
Sathvik_Bulusu <bulususathvik7890@gmail.com>
2024-02-22 13:52:44 +0530

    the java basics constructors

---
+++ hiie.java
@@ -0,0 +1,9 @@
+public class hiie {
+    int a;
+    int b;
+
+    public static void main(String[] args) {
+        hiie h = new hiie();// anonymous object or constructor
+        System.out.println(h.b+h.a);
+    }
+}
