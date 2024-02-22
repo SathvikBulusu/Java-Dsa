31fbb94aedb783219fd9071573fc13b2a91c5ada
Sathvik_Bulusu <bulususathvik7890@gmail.com>
2024-02-22 13:38:51 +0530

    the java basics

---
+++ etljnb.java
@@ -0,0 +1,24 @@
+public class etljnb {
+    int x = 5;
+    // having a static method wont work
+    void show(){
+        System.out.println(x);
+    }
+
+    // this acess modifie is used to specify access only with in the package
+    // this cannot be used with a static method
+
+    //public
+    //private
+    //default
+    //protected
+    // 4 types of access given to the java programs
+
+    // Constructor , it has same name as a class name and it is implicitly called when the object is created
+    // it is also called as special type of method which is used to initialize the object
+
+    // a constructor  must have no explicit return type and should not have any return type ) and the main purpose is to
+    // intialise the instance variables thats why constructor is used..
+
+
+}
